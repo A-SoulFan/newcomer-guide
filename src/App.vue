7132b00bd1cb90edbf37ba8cb085c6c8702eccf7
幻樱 <xiebo121314@gmail.com>
2021-11-18 20:15:07 +0800

    <feat>:新增根据首次加载窗口宽度跳转手机端

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,19 @@
     <router-view/>
   </div>
 </template>
+<script>
+export default {
+  name: "app",
+  created() {
+    window.addEventListener('load',() => {
+      const width = document.documentElement.clientWidth || document.body.offsetWidth || window.innerWidth || window.outerWidth
+      if (width <= 768) {
+        window.location.replace('https://mguide.asoulfan.com/')
+      }
+    })
+  }
+}
+</script>
 
 <style lang="scss">
 
