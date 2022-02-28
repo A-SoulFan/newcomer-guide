a0bf581fc9089f483ad4c271beb06606dcade02c
MilkTea <MilkTea@email>
2022-02-28 21:04:04 +0000

    <feat>: 增加团队作品轮播图

--- src/views/team.vue
+++ src/views/team.vue
@@ -1,81 +1,23 @@
 <template>
   <div class="team">
-    <div class="box">
-      <div class="title">{{ text }}</div>
-      <asheader />
-      <swiper ref="mySwiper" :options="swiperOption">
-        <swiper-slide>
-          <navbar />
-        </swiper-slide>
-        <swiper-slide>
-          <persona />
-        </swiper-slide>
-        <swiper-slide>
-          <recommendVideo />
-        </swiper-slide>
-      </swiper>
-    </div>
+    <teamWorks />
   </div>
 </template>
 
 <script>
-import asheader from "../components/header/header"
-import navbar from "../components/navbar/navbar"
-import persona from "../components/persona/persona"
-import relatedEntries from '../components/relatedEntries/relatedEntries'
-import personalWorks from "../components/personalWorks/personalWorks"
-import recommendVideo from "../components/recommendVideo/recommendVideo"
-const fortest = 0
+import teamWorks from "../components/teamWorks/teamWorks"
 export default {
 
   name: 'team',
   components: {
-    asheader,
-    navbar,
-    persona,
-    recommendVideo
+    teamWorks,
   },
   data () {
-    const vue = this
     return {
-      text: 'A-SOUL时代，沸腾期待！',
-      swiperOption: {
-        // effect: 'cube',
-        mousewheel: true,
-        // loop: true,
-        width: window.innerWidth,
-        on: {
-          slideChange: function () {
-            // console.log(this.realIndex)
-            vue.changeSlide(this.realIndex)
-          }
-        }
-      }
-    }
-  },
-  methods: {
-    changeSlide (index) {
-      // console.log(index)
-      // this.text = index
-      if (index == 0)
-        this.text = '团队简介'
-      else if (index == 1)
-        this.text = '成员介绍'
-      else
-        this.text = ''
     }
   }
 }
 </script>
-<style lang="scss" scoped>
-.title {
-  margin-top: 5vh;
-  width: 100%;
-  color: #fff;
-  position: fixed;
-  font-size: 5vh;
-  z-index: 22;
-  text-align: center;
-}
+<style>
 </style>
 
